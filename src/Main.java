53465e8c988b0c7718b5e92000493658bfff988d
adymar73583418 <adymarr2005@gmail.com>
2022-11-16 19:01:55 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        String myHobby;
+        final int NUM =24;
+        String word = "dog";
+        myHobby = NUM + word;
+        System.out.println(myHobby);
+        if (NUM<0){
+            System.out.println("вы сохронили отрицательное число");
+        }else if (NUM>0){
+            System.out.println("вы сохронили положительное число");
+        }else {
+            System.out.println("вы сохронили ноль");
+        }
+    }
+    }
